577441bd84a6437472ba25d502baedd1d263b4dd
Katrina Curro <kcurr3@unh.newhaven.edu>
2021-03-04 21:28:58 -0500

    Initial Commit

---
+++ Recreationalist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recreationalist
+//
+//  Created by Katrina Curro on 3/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recreationalist/RecreationalistApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecreationalistApp.swift
+//  Recreationalist
+//
+//  Created by Katrina Curro on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct RecreationalistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
