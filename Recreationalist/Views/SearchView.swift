45ca8d84c1cc5894092b0041a26ca370532b0fef
Katrina Curro <kcurr3@unh.newhaven.edu>
2021-03-24 16:30:02 -0400

    updating search page and search bar

--- Recreationalist/Views/SearchView.swift
+++ Recreationalist/Views/SearchView.swift
@@ -14,12 +14,28 @@ struct SearchView: View {
 
     
     var body: some View {
-        VStack(alignment: .center, spacing:30) {
-            Text("Recreationalist")
-                .font(.system(size:40))
-                .bold()
-            //NavigationView {
-                TextField("Search for a recreational activity...", text: $search)
+        //VStack(alignment: .center, spacing:30) {
+          //  Text("Recreationalist")
+            //    .font(.system(size:40))
+              //  .bold()
+        NavigationView {
+            ScrollView{
+                HStack {
+                    TextField("Search for a recreational activity...", text: $search)
+                }
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .disableAutocorrection(true)
+                    .padding(.horizontal)
+
+            }
+                .navigationBarTitle("Recreationalist")
+        }
+    }
+}
+                /*TextField("Search for a recreational activity...", text: $search)
+                    //added padding here
+                    .padding()
                     .background(Color(.secondarySystemBackground))
                     .disableAutocorrection(true)
                     .onTapGesture {
@@ -27,10 +43,10 @@ struct SearchView: View {
                     }
                 if self.editing{
                     //add functionality to clear search when editing is set to true aka user clicks text field
-                }
-            //}
-            Button(action: {
-                //add a query here to filter activities based on keyword trail/hike
+                }*/
+
+            /*Button(action: {
+                //add a query here to filter activities based on keyword park
             }) {
                 Text("Trails")
                     .font(.system(size: 18))
@@ -64,14 +80,13 @@ struct SearchView: View {
             }) {
                 Text("Classes")
                     .font(.system(size: 18))
-            }
-        }
-        .padding(30)
-    }
-}
+            }*/
+       // }
+   // }
+//}
 
-struct SearchView_Previews: PreviewProvider {
+/*struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
         SearchView()
     }
-}
+}*/
