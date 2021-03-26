c21964a99dbfc5bc043e999464b52d2d3d2354e9
Katrina Curro <kcurr3@unh.newhaven.edu>
2021-03-25 23:18:16 -0400

    updated formatting for nav links

--- Recreationalist/Views/SearchView.swift
+++ Recreationalist/Views/SearchView.swift
@@ -12,13 +12,14 @@ struct SearchView: View {
     @State var search: String = ""
     @State var choice = 0
 
-    var searchOptions: [String] = ["Trails", "Parks", "Gyms", "Courts & Fields", "Pools", "Classes"]
+    //var searchOptions: [String] = ["Trails", "Parks", "Gyms", "Courts & Fields", "Pools", "Classes"]
     
     var body: some View {
-        //VStack(alignment: .center, spacing:30) {
-          //  Text("Recreationalist")
-            //    .font(.system(size:40))
-              //  .bold()
+        /*VStack(alignment: .center, spacing:30) {
+            Text("Recreationalist")
+                .font(.system(size:40))
+                .bold()*/
+        
         NavigationView {
             ScrollView{
                 HStack {
@@ -33,87 +34,62 @@ struct SearchView: View {
                         }) {
                             Text("Clear")
                         }
+                        //added for time being
+                        Button(action: {
+                            print("Floating Button Click")
+                        }, label: {
+                            NavigationLink(destination: SiteListView()) {
+                                 Text("Search")
+                             }
+                        })
                     }
                 } .padding()
-                //if self.search != "" {
-                //    Text("No results")
-                //} else {
-                    //SiteListView()
-                //}
+
                 VStack{
-                    /*
-                Button(action: {
-                    print("Floating Button Click")
-                }, label: {
-                    NavigationLink(destination: SiteListView()) {
-                         Text("Trails")
-                     }
-                })
-                Button(action: {
-                    print("Floating Button Click")
-                }, label: {
-                    NavigationLink(destination: SiteListView()) {
-                         Text("Parks")
-                     }
-                })
-
-                Button(action: {
-                    print("Floating Button Click")
-                }, label: {
-                    NavigationLink(destination: SiteListView()) {
-                         Text("Gyms")
-                     }
-                })
-
-                Button(action: {
-                    print("Floating Button Click")
-                }, label: {
-                    NavigationLink(destination: SiteListView()) {
-                         Text("Courts & Fields")
-                     }
-                })
-
-                Button(action: {
-                    print("Floating Button Click")
-                }, label: {
-                    NavigationLink(destination: SiteListView()) {
-                         Text("Pools")
-                     }
-                })
-
-                Button(action: {
-                    print("Floating Button Click")
-                }, label: {
-                    NavigationLink(destination: SiteListView()) {
-                         Text("Classes")
-                     }
-                })*/
                     NavigationLink(destination: SiteListView()) {
                         Text("Trails")
+                            .font(.system(size:20))
+
                     }
                     NavigationLink(destination: SiteListView()) {
                         Text("Parks")
-                    }
-                    NavigationLink(destination: SiteListView()) {
-                        Text("Gyms")
-                    }
-                    NavigationLink(destination: SiteListView()) {
-                        Text("Courts & Fields")
-                    }
-                    NavigationLink(destination: SiteListView()) {
-                        Text("Pools")
-                    }
-                    NavigationLink(destination: SiteListView()) {
-                        Text("Classes")
+                            .font(.system(size:20))
+
                     }
 
+                    NavigationLink(destination: SiteListView()) {
+                        Text("Gyms")
+                            .font(.system(size:20))
+
+                    }
+
+                    NavigationLink(destination: SiteListView()) {
+                        Text("Courts & Fields")
+                            .font(.system(size:20))
+
+                    }
+
+                    NavigationLink(destination: SiteListView()) {
+                        Text("Pools")
+                            .font(.system(size:20))
+
+                    }
+
+                    NavigationLink(destination: SiteListView()) {
+                        Text("Classes")
+                            .font(.system(size:20))
+
+                    }
                 } .padding()
             }
                 .navigationBarTitle("Recreationalist")
         }
     }
 }
-                /*TextField("Search for a recreational activity...", text: $search)
+                
+
+
+/*TextField("Search for a recreational activity...", text: $search)
                     //added padding here
                     .padding()
                     .background(Color(.secondarySystemBackground))
@@ -123,47 +99,58 @@ struct SearchView: View {
                     }
                 if self.editing{
                     //add functionality to clear search when editing is set to true aka user clicks text field
-                }*/
+ 
+ 
+ 
+ /*
+Button(action: {
+ print("Floating Button Click")
+}, label: {
+ NavigationLink(destination: SiteListView()) {
+      Text("Trails")
+  }
+})
+Button(action: {
+ print("Floating Button Click")
+}, label: {
+ NavigationLink(destination: SiteListView()) {
+      Text("Parks")
+  }
+})
 
-            /*Button(action: {
-                //add a query here to filter activities based on keyword park
-            }) {
-                Text("Trails")
-                    .font(.system(size: 18))
-            }
-            Button(action: {
-                //add a query here to filter activities based on keyword park
-            }) {
-                Text("Parks")
-                .font(.system(size: 18))
-            }
-            Button(action: {
-                //add a query here to filter activities based on keyword gym
-            }) {
-                Text("Gyms")
-                    .font(.system(size: 18))
-            }
-            Button(action: {
-                //add a query here to filter activities based on keyword court/field
-            }) {
-                Text("Courts & Fields")
-                    .font(.system(size: 18))
-            }
-            Button(action: {
-                //add a query here to filter activities based on keyword pools
-            }) {
-                Text("Pools")
-                    .font(.system(size: 18))
-            }
-            Button(action: {
-                    //add a query here to filter activities based on keyword classes
-            }) {
-                Text("Classes")
-                    .font(.system(size: 18))
-            }*/
-       // }
-   // }
-//}
+Button(action: {
+ print("Floating Button Click")
+}, label: {
+ NavigationLink(destination: SiteListView()) {
+      Text("Gyms")
+  }
+})
+
+Button(action: {
+ print("Floating Button Click")
+}, label: {
+ NavigationLink(destination: SiteListView()) {
+      Text("Courts & Fields")
+  }
+})
+
+Button(action: {
+ print("Floating Button Click")
+}, label: {
+ NavigationLink(destination: SiteListView()) {
+      Text("Pools")
+  }
+})
+
+Button(action: {
+ print("Floating Button Click")
+}, label: {
+ NavigationLink(destination: SiteListView()) {
+      Text("Classes")
+  }
+})*/
+ 
+                }*/
 
 /*struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
