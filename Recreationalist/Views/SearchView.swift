3abbe2d3fb59d77d2dded19ed4d40ed127e1b968
Katrina Curro <kcurr3@unh.newhaven.edu>
2021-04-06 15:27:05 -0400

    updating formatting on search page

--- Recreationalist/Views/SearchView.swift
+++ Recreationalist/Views/SearchView.swift
@@ -43,7 +43,8 @@ struct SearchView: View {
                                 }
                             })
                         }
-                    } .padding()
+                    }
+                    .padding()
                 }
                 .navigationBarTitle("Recreationalist")
             }
