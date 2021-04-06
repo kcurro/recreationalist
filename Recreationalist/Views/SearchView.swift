f373f9a6ea61f2b8d66d4886b60092a73eab6211
Katrina Curro <kcurr3@unh.newhaven.edu>
2021-04-06 15:19:28 -0400

    adding background image to search page

--- Recreationalist/Views/SearchView.swift
+++ Recreationalist/Views/SearchView.swift
@@ -15,97 +15,38 @@ struct SearchView: View {
         
     var body: some View {
         NavigationView {
-            VStack(alignment: .center, spacing:30) {
-                Text("Search for Recreational Activites")
-                    .font(.system(size:25))
-                HStack {
-                    TextField("Search by city...", text: $search)
-                        .padding()
-                        .background(Color(.secondarySystemBackground))
-                        .disableAutocorrection(true)
-                        .padding(.horizontal)
-                if self.search != "" {
-                    Button(action: {
-                        self.search = ""
-                    }) {
-                        Text("Clear")
-                    }
-                    Button(action: {
-                        print("Floating Button Click");
-                        self.selection = "User Search"
-                    }, label: {
-                        NavigationLink(destination: UserSearch() , tag: "User Search", selection: $selection) {
-                            Text("Search")
+            ZStack{
+                Image("hiking")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                VStack(alignment: .center, spacing:30) {
+                    Text("Search for Recreational Activites")
+                        .font(.system(size:25))
+                    HStack {
+                        TextField("Search by city...", text: $search)
+                            .padding()
+                            .background(Color(.secondarySystemBackground))
+                            .disableAutocorrection(true)
+                            .padding(.horizontal)
+                        if self.search != "" {
+                            Button(action: {
+                                self.search = ""
+                            }) {
+                                Text("Clear")
                             }
-                        })
-                    }
-                } .padding()
-                // to do ** change these to filter options
-                /*
-                Text("Sites by Activity Near Me")
-                    .font(.system(size:25))
-                
-                Button(action: {
-                    print("Trails Button Click");
-                    self.selection = "Trails Only"
-                }, label: {
-                    NavigationLink(destination: TrailsView(), tag: "Trails Only", selection: $selection){
-                        Text("Trails")
-                            .font(.system(size:20))
-                    }
-                })
-                    
-                Button(action: {
-                    print("Parks Button Click");
-                    self.selection = "Parks Only"
-                }, label: {
-                    NavigationLink(destination: ParksView(), tag: "Parks Only", selection: $selection){
-                        Text("Parks")
-                            .font(.system(size:20))
-                    }
-                })
-                    
-                Button(action: {
-                    print("Gym Button Click");
-                    self.selection = "Gyms Only"
-                }, label: {
-                    NavigationLink(destination: GymView(), tag: "Gyms Only", selection: $selection){
-                        Text("Gyms")
-                            .font(.system(size:20))
-                    }
-                })
-                    
-                Button(action: {
-                    print("Fields Button Click");
-                    self.selection = "Fields Only"
-                }, label: {
-                    NavigationLink(destination: FieldsView(), tag: "Fields Only", selection: $selection){
-                        Text("Courts & Fields")
-                            .font(.system(size:20))
-                    }
-                })
-                    
-                Button(action: {
-                    print("Pools Button Click");
-                    self.selection = "Pools Only"
-                }, label: {
-                    NavigationLink(destination: PoolsView(), tag: "Pools Only", selection: $selection){
-                        Text("Pools")
-                            .font(.system(size:20))
-                    }
-                })
-                                        
-                Button(action: {
-                    print("Classes Button Click");
-                    self.selection = "Classes Only"
-                }, label: {
-                    NavigationLink(destination: ClassesView(), tag: "Classes Only", selection: $selection){
-                        Text("Classes")
-                            .font(.system(size:20))
-                    }
-                })*/
+                            Button(action: {
+                                print("Floating Button Click");
+                                self.selection = "User Search"
+                            }, label: {
+                                NavigationLink(destination: UserSearch() , tag: "User Search", selection: $selection) {
+                                    Text("Search")
+                                }
+                            })
+                        }
+                    } .padding()
+                }
+                .navigationBarTitle("Recreationalist")
             }
-            .navigationBarTitle("Recreationalist")
         }
     }
 }
