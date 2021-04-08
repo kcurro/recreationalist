0a774e18c3def0b7199ad2e64e04a8a85a70163a
KimGovea <78005729+KimGovea@users.noreply.github.com>
2021-04-08 14:34:36 -0700

    FirebaseSession, AuthenticationView, MoreView, and ProfileView
    I updated/created new files in order to create the sign in/sign up page. Futhermore, RecreationalistApp.swift was also updated to include the firebase session. As mentioned, the app works as long as one SIGNS OUT before attempting to run again. I suspect the culprit is @ViewBuilder

--- Recreationalist/Views/MoreOption-LogOut.swift
+++ Recreationalist/Views/MoreOption-LogOut.swift
@@ -10,13 +10,20 @@ import SwiftUI
 struct MoreOption_LogOut: View {
     //MARK: -PROPERTIES
     @State private var logOutAlert = false
+    @EnvironmentObject var session: FirebaseSession
     
     //MARK: -BODY
     var body: some View {
-        Button("Log Out") {
-            logOutAlert = true 
-        }
-        .alert(isPresented: $logOutAlert) { () -> Alert in Alert(title: Text("Okay, you're logged out!"))
+        /*Button("Log Out") {
+            logOutAlert = true
+            
+        }*/
+        
+        Button(action: {session.signOut(); logOutAlert=true}, label: {
+            Text("Sign Out")
+        })
+        
+        .alert(isPresented: $logOutAlert) { () -> Alert in Alert(title: Text("Okay, you're signed out!"))
         }
         .foregroundColor(Color.black)
         .buttonStyle(SemiboldButtonFont())
@@ -36,5 +43,6 @@ struct MoreOption_LogOut_Previews: PreviewProvider {
         MoreOption_LogOut()
             .previewLayout(.fixed(width:375, height:60))
             .padding()
+            .environmentObject(FirebaseSession())
     }
 }
--- Recreationalist/Views/MoreView.swift
+++ Recreationalist/Views/MoreView.swift
@@ -7,9 +7,25 @@
 
 import SwiftUI
 
+//MARK: -MORE VIEW
 struct MoreView: View {
-    // MARK: - PROPERTIES
+    //MARK: -PROPERTIES
+    @EnvironmentObject var session: FirebaseSession
     
+    //MARK: -BODY
+    @ViewBuilder
+    var body: some View {
+        if session.isSignedIn {
+            MoreSignedInView().environmentObject(session)
+        } else {
+            MoreSignedOutView().environmentObject(session)
+        }
+    }
+}
+
+struct MoreSignedInView: View {
+    // MARK: - PROPERTIES
+    @EnvironmentObject var session: FirebaseSession
     // MARK: - BODY
     var body: some View {
         NavigationView {
@@ -64,10 +80,118 @@ struct MoreView: View {
     }
 }
 
-// MARK:  - PREVIEW
+struct MoreSignedOutView: View {
+    // MARK: - PROPERTIES
+    @EnvironmentObject var session: FirebaseSession
+    // MARK: - BODY
+    var body: some View {
+        NavigationView {
+            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 0)  {
+                // MARK: - FORM
+                Form{
+                    // MARK: - SECTION 4
+                    Section(header: Text("Application Settings")) {
+                        NavigationLink(destination: MoreOption_PushNotifications()){
+                            Text("Push Notifications")
+                                .fontWeight(.semibold)
+                        }
+                        NavigationLink(destination: MoreOption_MyLocation()){
+                            Text("My Location")
+                                .fontWeight(.semibold)
+                        }
+                        NavigationLink(destination: MoreOption_LocationServices()){
+                            MoreRow(firstText: "Location Services")
+                        }
+                        NavigationLink(destination: ProfileView()){
+                            MoreRow(firstText: "Sign In")
+                        }
+                    } //:SECTION 4
+                    .padding(.vertical, 3)
+                }//: FORM
+                
+                .listStyle(GroupedListStyle())
+                .environment(\.horizontalSizeClass, .regular)
+                
+                // MARK: - FOOTER
+                Text("Terms of Service, Privacy Policy\nCopyright © 2021 \nRecreationalist Version 1.0.0 ")
+                    .multilineTextAlignment(.center)
+                    .font(.footnote)
+                    .padding(.top,6)
+                    .padding(.bottom,8)
+                    .foregroundColor(Color.secondary)
+            }//: VSTACK
+            .navigationBarTitle("More", displayMode: .inline)
+            .background(Color("ColorBackground").edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/))
+        }//: NAVIGATION
+    }
+}
+
+struct MoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoreView().environmentObject(FirebaseSession())
+    }
+}
+
+/*//MARK: -MORE VIEW
+struct MoreView: View {
+    // MARK: - PROPERTIES
+    @EnvironmentObject var session: FirebaseSession
+    // MARK: - BODY
+    var body: some View {
+        NavigationView {
+            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 0)  {
+                // MARK: - FORM
+                Form{
+                    // MARK: - SECTION 4
+                    Section(header: Text("Application Settings")) {
+                        NavigationLink(destination: MoreOption_EmailNotifications()){
+                            Text("Email Notifications")
+                                .fontWeight(.semibold)
+                        }
+                        NavigationLink(destination: MoreOption_PushNotifications()){
+                            Text("Push Notifications")
+                                .fontWeight(.semibold)
+                        }
+                        NavigationLink(destination: MoreOption_LinkedAccounts()){
+                            Text("Linked Accounts")
+                                .fontWeight(.semibold)
+                        }
+                        NavigationLink(destination: MoreOption_PrivacySettings()){
+                            Text("Privacy Settings")
+                                .fontWeight(.semibold)
+                        }
+                        NavigationLink(destination: MoreOption_MyLocation()){
+                            Text("My Location")
+                                .fontWeight(.semibold)
+                        }
+                        NavigationLink(destination: MoreOption_LocationServices()){
+                            MoreRow(firstText: "Location Services")
+                        }
+                        MoreOption_ClearHistory()
+                        MoreOption_LogOut()
+                    } //:SECTION 4
+                    .padding(.vertical, 3)
+                }//: FORM
+                
+                .listStyle(GroupedListStyle())
+                .environment(\.horizontalSizeClass, .regular)
+                
+                // MARK: - FOOTER
+                Text("Terms of Service, Privacy Policy\nCopyright © 2021 \nRecreationalist Version 1.0.0 ")
+                    .multilineTextAlignment(.center)
+                    .font(.footnote)
+                    .padding(.top,6)
+                    .padding(.bottom,8)
+                    .foregroundColor(Color.secondary)
+            }//: VSTACK
+            .navigationBarTitle("More", displayMode: .inline)
+            .background(Color("ColorBackground").edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/))
+        }//: NAVIGATION
+    }
+}
 
 struct MoreView_Previews: PreviewProvider {
     static var previews: some View {
         MoreView()
     }
-}
+}*/
