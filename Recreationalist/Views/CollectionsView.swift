63d04f107410a1132a47d02605ba41190174d1ff
Katrina Curro <kcurr3@unh.newhaven.edu>
2021-04-15 20:50:14 -0400

    creating framework for collection page

--- Recreationalist/Views/CollectionsView.swift
+++ Recreationalist/Views/CollectionsView.swift
@@ -6,10 +6,35 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
+
+//update with correct firebase collection thinking this will be done on the user level collection
+//let trailsCollectionRef = Firestore.firestore().collection("recSites")
 
 struct CollectionsView: View {
+    //update with correct firebase collection thinking this will be done on the user level collection
+    //@ObservedObject private var sites: FirebaseCollection<Site>
+    
+    //private var sitesQuery: Query
+        
+    init() {
+        /*self.sitesQuery = parksCollectionRef.whereField("parks", isEqualTo: true).order(by: "name")
+            
+        self.sites = FirebaseCollection<Site>(query: sitesQuery)*/
+    }
+    
     var body: some View {
-        Text("Collections Page TBD")
+        List{
+            //update with correct firebase collection thinking this will be done on the user level collection
+            
+            /*ForEach(sites.items) {
+                site in NavigationLink(destination: SiteDetailView(site: site)) {
+                    SiteRow(site: site)
+                }
+            }*/
+            
+        }
+        .navigationBarTitle("Saved Collection", displayMode: .inline)
     }
 }
 
