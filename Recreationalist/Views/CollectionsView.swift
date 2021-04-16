e9366be35ec8983da22ca37725d10d0fbc2f62ce
Katrina Curro <kcurr3@unh.newhaven.edu>
2021-04-15 22:24:17 -0400

    updating framework for collections page

--- Recreationalist/Views/CollectionsView.swift
+++ Recreationalist/Views/CollectionsView.swift
@@ -8,38 +8,59 @@
 import SwiftUI
 import FirebaseFirestore
 
+//currently this page is loading with all rec site data...
 //update with correct firebase collection thinking this will be done on the user level collection
-//let trailsCollectionRef = Firestore.firestore().collection("recSites")
+let sitesCollectionRef = Firestore.firestore().collection("recSites")
 
 struct CollectionsView: View {
-    //update with correct firebase collection thinking this will be done on the user level collection
-    //@ObservedObject private var sites: FirebaseCollection<Site>
-    
-    //private var sitesQuery: Query
-        
-    init() {
-        /*self.sitesQuery = parksCollectionRef.whereField("parks", isEqualTo: true).order(by: "name")
-            
-        self.sites = FirebaseCollection<Site>(query: sitesQuery)*/
-    }
+    @EnvironmentObject var appState: AppState
+    @EnvironmentObject var session: FirebaseSession
     
     var body: some View {
-        List{
-            //update with correct firebase collection thinking this will be done on the user level collection
-            
-            /*ForEach(sites.items) {
-                site in NavigationLink(destination: SiteDetailView(site: site)) {
-                    SiteRow(site: site)
+        NavigationView {
+            Group {
+                if (session.isSignedIn) {
+                    /*CollectionInternalView(sites: FirebaseCollection<Site>(query: sitesCollectionRef.whereField("user_id", isEqualTo: session.user?.uid ?? "nil").order(by: "timestamp", descending: true)))*/
+                    CollectionInternalView(sites: FirebaseCollection<Site>(query: sitesCollectionRef.order(by: "name")))
+                } else {
+                    Button("Sign in to view saved activities") {
+                        appState.selectedOption = Tab.profile
+                    }
+                    .font(.headline)
                 }
-            }*/
-            
+            }
+            .navigationBarTitle("Saved Activities", displayMode: .inline)
         }
-        .navigationBarTitle("Saved Collection", displayMode: .inline)
+    }
+
+    
+    struct CollectionInternalView: View {
+        @ObservedObject private var sites: FirebaseCollection<Site>
+        
+            //update init to sites
+        init(sites: FirebaseCollection<Site>) {
+            //update with correct firebase collection thinking this will be done on the user level collection
+            //self.saved = saved
+            self.sites = sites
+        }
+        
+        var body: some View {
+            //update with correct firebase collection thinking this will be done on the user level collection
+            List{
+                ForEach(sites.items) {
+                    site in NavigationLink(destination: SiteDetailView(site: site)) {
+                        SiteRow(site: site)
+                    }
+                }
+            }
+        }
     }
 }
 
+
+/*
 struct CollectionsView_Previews: PreviewProvider {
     static var previews: some View {
         CollectionsView()
     }
-}
+}*/
