d639e857c0ec5a967c7a386dd6a8564b4b845bc1
Katrina Curro <kcurr3@unh.newhaven.edu>
2021-04-27 22:07:49 -0400

    updating comments

--- Recreationalist/Views/UsersSearch.swift
+++ Recreationalist/Views/UsersSearch.swift
@@ -123,9 +123,8 @@ struct TrailsView: View {
         
     var location: CLLocationCoordinate2D
 
-        
+        //reference code for formula https://stackoverflow.com/questions/56674470/flutter-firestore-geo-query
     init(location: CLLocationCoordinate2D) {
-        //self.location = locationManager.location != nil ? locationManager.location!.coordinate : CLLocationCoordinate2D()
         self.location = location
         print("coordinate in site list init view \(self.location)")
         
@@ -188,7 +187,6 @@ struct ParksView: View {
 
         
     init(location: CLLocationCoordinate2D) {
-        //self.location = locationManager.location != nil ? locationManager.location!.coordinate : CLLocationCoordinate2D()
         self.location = location
         print("coordinate in site list init view \(self.location)")
         
